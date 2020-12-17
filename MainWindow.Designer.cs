23b089e5bfc20dd8f1bb817aa35cd002fed1451f
JaredDC <jared.dc@outlook.com>
2020-12-17 17:42:20 +0800

    改名

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -28,145 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tbAOFolder = new System.Windows.Forms.TextBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.btnSelectOutFolder = new System.Windows.Forms.Button();
-            this.btnPrintOutFolder = new System.Windows.Forms.Button();
-            this.tbOutFolder = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.btnSelectFromRegistry = new System.Windows.Forms.Button();
-            this.btnSelectAOFolder = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBoxTarget = new System.Windows.Forms.GroupBox();
+            this.btnSelectTargetFolder = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.tbTargetFolder = new System.Windows.Forms.TextBox();
+            this.labelTarget = new System.Windows.Forms.Label();
+            this.groupBoxConsole = new System.Windows.Forms.GroupBox();
             this.tbConsole = new System.Windows.Forms.TextBox();
-            this.btnExtract = new System.Windows.Forms.Button();
+            this.btnClean = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
             this.btnStop = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
-            this.groupBox3.SuspendLayout();
+            this.groupBoxTarget.SuspendLayout();
+            this.groupBoxConsole.SuspendLayout();
             this.SuspendLayout();
+
             // 
-            // tbAOFolder
+            // groupBoxTarget
             // 
-            this.tbAOFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.groupBoxTarget.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbAOFolder.Location = new System.Drawing.Point(142, 64);
-            this.tbAOFolder.Margin = new System.Windows.Forms.Padding(4);
-            this.tbAOFolder.Name = "tbAOFolder";
-            this.tbAOFolder.Size = new System.Drawing.Size(592, 28);
-            this.tbAOFolder.TabIndex = 0;
+            this.groupBoxTarget.Controls.Add(this.btnSelectTargetFolder);
+            this.groupBoxTarget.Controls.Add(this.btnPrint);
+            this.groupBoxTarget.Controls.Add(this.tbTargetFolder);
+            this.groupBoxTarget.Controls.Add(this.labelTarget);
+            this.groupBoxTarget.Location = new System.Drawing.Point(9, 1);
+            this.groupBoxTarget.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBoxTarget.Name = "groupBoxTarget";
+            this.groupBoxTarget.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBoxTarget.Size = new System.Drawing.Size(1010, 80);
+            this.groupBoxTarget.TabIndex = 1;
+            this.groupBoxTarget.TabStop = false;
+            this.groupBoxTarget.Text = "Paths";
             // 
-            // groupBox1
+            // btnSelectTargetFolder
             // 
-            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.btnSelectTargetFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectTargetFolder.Location = new System.Drawing.Point(830, 24);
+            this.btnSelectTargetFolder.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSelectTargetFolder.Name = "btnSelectTargetFolder";
+            this.btnSelectTargetFolder.Size = new System.Drawing.Size(80, 32);
+            this.btnSelectTargetFolder.TabIndex = 5;
+            this.btnSelectTargetFolder.Text = "Select";
+            this.btnSelectTargetFolder.UseVisualStyleBackColor = true;
+            this.btnSelectTargetFolder.Click += new System.EventHandler(this.btnSelectOutFolder_Click);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPrint.Location = new System.Drawing.Point(920, 24);
+            this.btnPrint.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(80, 32);
+            this.btnPrint.TabIndex = 5;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // tbTargetFolder
+            // 
+            this.tbTargetFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox1.Controls.Add(this.btnSelectOutFolder);
-            this.groupBox1.Controls.Add(this.btnPrintOutFolder);
-            this.groupBox1.Controls.Add(this.tbOutFolder);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(9, 1);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox1.Size = new System.Drawing.Size(1010, 80);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Paths";
+            this.tbTargetFolder.Location = new System.Drawing.Point(142, 26);
+            this.tbTargetFolder.Margin = new System.Windows.Forms.Padding(4);
+            this.tbTargetFolder.Name = "tbTargetFolder";
+            this.tbTargetFolder.Size = new System.Drawing.Size(680, 28);
+            this.tbTargetFolder.TabIndex = 3;
+            this.tbTargetFolder.TextChanged += new System.EventHandler(this.tbTargetFolder_TextChanged);
             // 
-            // btnSelectOutFolder
+            // labelTarget
             // 
-            this.btnSelectOutFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSelectOutFolder.Location = new System.Drawing.Point(830, 24);
-            this.btnSelectOutFolder.Margin = new System.Windows.Forms.Padding(4);
-            this.btnSelectOutFolder.Name = "btnSelectOutFolder";
-            this.btnSelectOutFolder.Size = new System.Drawing.Size(80, 32);
-            this.btnSelectOutFolder.TabIndex = 5;
-            this.btnSelectOutFolder.Text = "Select";
-            this.btnSelectOutFolder.UseVisualStyleBackColor = true;
-            this.btnSelectOutFolder.Click += new System.EventHandler(this.btnSelectOutFolder_Click);
+            this.labelTarget.AutoSize = true;
+            this.labelTarget.Location = new System.Drawing.Point(9, 30);
+            this.labelTarget.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelTarget.Name = "labelTarget";
+            this.labelTarget.Size = new System.Drawing.Size(134, 18);
+            this.labelTarget.TabIndex = 1;
+            this.labelTarget.Text = "Target Folder:";
+
             // 
-            // btnPrintOutFolder
+            // groupBoxConsole
             // 
-            this.btnPrintOutFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnPrintOutFolder.Location = new System.Drawing.Point(920, 24);
-            this.btnPrintOutFolder.Margin = new System.Windows.Forms.Padding(4);
-            this.btnPrintOutFolder.Name = "btnPrintOutFolder";
-            this.btnPrintOutFolder.Size = new System.Drawing.Size(80, 32);
-            this.btnPrintOutFolder.TabIndex = 5;
-            this.btnPrintOutFolder.Text = "Print";
-            this.btnPrintOutFolder.UseVisualStyleBackColor = true;
-            this.btnPrintOutFolder.Click += new System.EventHandler(this.btnPrint_Click);
-            // 
-            // tbOutFolder
-            // 
-            this.tbOutFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbOutFolder.Location = new System.Drawing.Point(142, 26);
-            this.tbOutFolder.Margin = new System.Windows.Forms.Padding(4);
-            this.tbOutFolder.Name = "tbOutFolder";
-            this.tbOutFolder.Size = new System.Drawing.Size(680, 28);
-            this.tbOutFolder.TabIndex = 3;
-            this.tbOutFolder.TextChanged += new System.EventHandler(this.tbOutFolder_TextChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 30);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(134, 18);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Target Folder:";
-            // 
-            // btnSelectFromRegistry
-            // 
-            this.btnSelectFromRegistry.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSelectFromRegistry.Location = new System.Drawing.Point(746, 61);
-            this.btnSelectFromRegistry.Margin = new System.Windows.Forms.Padding(4);
-            this.btnSelectFromRegistry.Name = "btnSelectFromRegistry";
-            this.btnSelectFromRegistry.Size = new System.Drawing.Size(134, 32);
-            this.btnSelectFromRegistry.TabIndex = 7;
-            this.btnSelectFromRegistry.Text = "From Registry";
-            this.btnSelectFromRegistry.UseVisualStyleBackColor = true;
-            this.btnSelectFromRegistry.Click += new System.EventHandler(this.btnSelectFromRegistry_Click);
-            // 
-            // btnSelectAOFolder
-            // 
-            this.btnSelectAOFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSelectAOFolder.Location = new System.Drawing.Point(888, 61);
-            this.btnSelectAOFolder.Margin = new System.Windows.Forms.Padding(4);
-            this.btnSelectAOFolder.Name = "btnSelectAOFolder";
-            this.btnSelectAOFolder.Size = new System.Drawing.Size(112, 32);
-            this.btnSelectAOFolder.TabIndex = 6;
-            this.btnSelectAOFolder.Text = "Select";
-            this.btnSelectAOFolder.UseVisualStyleBackColor = true;
-            this.btnSelectAOFolder.Click += new System.EventHandler(this.btnSelectAOFolder_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 68);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(143, 18);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "AO Main Folder:";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // groupBox3
-            // 
-            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.groupBoxConsole.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox3.Controls.Add(this.tbConsole);
-            this.groupBox3.Location = new System.Drawing.Point(9, 100);
-            this.groupBox3.Margin = new System.Windows.Forms.Padding(4);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox3.Size = new System.Drawing.Size(1010, 530);
-            this.groupBox3.TabIndex = 3;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Console";
+            this.groupBoxConsole.Controls.Add(this.tbConsole);
+            this.groupBoxConsole.Location = new System.Drawing.Point(9, 100);
+            this.groupBoxConsole.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBoxConsole.Name = "groupBoxConsole";
+            this.groupBoxConsole.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBoxConsole.Size = new System.Drawing.Size(1010, 530);
+            this.groupBoxConsole.TabIndex = 3;
+            this.groupBoxConsole.TabStop = false;
+            this.groupBoxConsole.Text = "Console";
             // 
             // tbConsole
             // 
@@ -183,17 +136,17 @@
             this.tbConsole.TabIndex = 0;
             this.tbConsole.WordWrap = false;
             // 
-            // btnExtract
+            // btnClean
             // 
-            this.btnExtract.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnExtract.Location = new System.Drawing.Point(897, 652);
-            this.btnExtract.Margin = new System.Windows.Forms.Padding(4);
-            this.btnExtract.Name = "btnExtract";
-            this.btnExtract.Size = new System.Drawing.Size(112, 32);
-            this.btnExtract.TabIndex = 4;
-            this.btnExtract.Text = "Clean";
-            this.btnExtract.UseVisualStyleBackColor = true;
-            this.btnExtract.Click += new System.EventHandler(this.btnExtract_Click);
+            this.btnClean.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClean.Location = new System.Drawing.Point(897, 652);
+            this.btnClean.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(112, 32);
+            this.btnClean.TabIndex = 4;
+            this.btnClean.Text = "Clean";
+            this.btnClean.UseVisualStyleBackColor = true;
+            this.btnClean.Click += new System.EventHandler(this.btnExtract_Click);
             // 
             // btnClear
             // 
@@ -224,39 +177,33 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1028, 701);
-            this.Controls.Add(this.btnExtract);
+            this.Controls.Add(this.btnClean);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnStop);
-            this.Controls.Add(this.groupBox3);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBoxConsole);
+            this.Controls.Add(this.groupBoxTarget);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MinimumSize = new System.Drawing.Size(754, 536);
             this.Name = "MainWindow";
             this.Text = "Empty Folder Deleter";
-            this.Load += new System.EventHandler(this.MainWindow_Load);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            this.groupBox3.ResumeLayout(false);
-            this.groupBox3.PerformLayout();
+            this.groupBoxTarget.ResumeLayout(false);
+            this.groupBoxTarget.PerformLayout();
+            this.groupBoxConsole.ResumeLayout(false);
+            this.groupBoxConsole.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox tbAOFolder;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox tbOutFolder;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnSelectAOFolder;
-        private System.Windows.Forms.Button btnSelectOutFolder;
-        private System.Windows.Forms.Button btnPrintOutFolder;
-        private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.Button btnExtract;
+        private System.Windows.Forms.GroupBox groupBoxTarget;
+        private System.Windows.Forms.TextBox tbTargetFolder;
+        private System.Windows.Forms.Label labelTarget;
+        private System.Windows.Forms.Button btnSelectTargetFolder;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.GroupBox groupBoxConsole;
+        private System.Windows.Forms.Button btnClean;
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Button btnStop;
-        private System.Windows.Forms.Button btnSelectFromRegistry;
         public System.Windows.Forms.TextBox tbConsole;
     }
 }
--- MainWindow.cs
+++ MainWindow.cs
@@ -12,33 +12,6 @@ namespace empty_folder_deleter
             InitializeComponent();
         }
 
-        private void MainWindow_Load(object sender, EventArgs e)
-        {
-        }
-
-        private void btnSelectFromRegistry_Click(object sender, EventArgs e)
-        {
-            string obj = (string)Registry.LocalMachine.OpenSubKey(@"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\SandboxAlbionOnline", false).GetValue("DisplayIcon");
-            Program.MainGameFolder = Path.Combine(Path.GetDirectoryName(obj.Trim('\"')), "..");
-
-            tbAOFolder.Text = Program.MainGameFolder;
-        }
-
-        private void btnSelectAOFolder_Click(object sender, EventArgs e)
-        {
-            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
-            folderBrowserDialog.ShowNewFolderButton = false;
-            folderBrowserDialog.SelectedPath = Path.GetDirectoryName(Application.ExecutablePath);
-            folderBrowserDialog.Description = "Please select the Albion Online Gamefolder. Example: C:\\AlbionOnline";
-            DialogResult res = folderBrowserDialog.ShowDialog();
-
-            if (res == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
-            {
-                Program.MainGameFolder = folderBrowserDialog.SelectedPath;
-                tbAOFolder.Text = Program.MainGameFolder;
-            }
-        }
-
         private void btnSelectOutFolder_Click(object sender, EventArgs e)
         {
             FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
@@ -50,7 +23,7 @@ namespace empty_folder_deleter
             if (res == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
             {
                 Program.OutputFolderPath = folderBrowserDialog.SelectedPath;
-                tbOutFolder.Text = Program.OutputFolderPath;
+                tbTargetFolder.Text = Program.OutputFolderPath;
             }
         }
 
@@ -82,10 +55,10 @@ namespace empty_folder_deleter
 
         }
 
-        private void tbOutFolder_TextChanged(object sender, EventArgs e)
+        private void tbTargetFolder_TextChanged(object sender, EventArgs e)
         {
-            Program.OutputFolderPath = tbOutFolder.Text;
-            //var newPath = "path change to:" + tbOutFolder.Text + "\r\n";
+            Program.OutputFolderPath = tbTargetFolder.Text;
+            //var newPath = "path change to:" + tbTargetFolder.Text + "\r\n";
             //this.tbConsole.Text += newPath;
         }
 
